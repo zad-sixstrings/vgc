4f3e68825b8df540438a5778991f4adae7eb44dc
Zad Sixstrings <toxxiczad@gmail.com>
2025-10-11 10:41:03 +0200

    Add files with dotenv support

---
+++ database.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+func dbconnect() {
+
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading.env file")
+	}
+
+	//Make connection string from .env
+	connStr := fmt.Sprintf(
+		"postgresql://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	conn, err := pgx.Connect(context.Background(), connStr)
+	if err != nil {
+		log.Fatal("Unable to connect to database:", err)
+	}
+
+	defer conn.Close(context.Background())
+
+}
