8ef01469af9925f6ddcd6cd402ed44ff1c5552a1
Zad Sixstrings <toxxiczad@gmail.com>
2025-10-16 18:23:05 +0200

    modularized and refactored

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"fyne.io/fyne/v2"
@@ -17,7 +16,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer conn.Close(context.Background()) // Close when main() exits
+	defer conn.Close(context.Background())
 
 	// Fetch data
 	games, err := getGames(conn)
@@ -34,118 +33,13 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("VGC")
 
-	// Build games table
-	gamesTable := widget.NewTableWithHeaders(
-		func() (int, int) {
-			return len(games), 4 // +1 for header row
-		},
-		func() fyne.CanvasObject {
-			return widget.NewLabel("wide content")
-		},
-		func(id widget.TableCellID, obj fyne.CanvasObject) {
-			label := obj.(*widget.Label)
-
-			// Data rows
-			game := games[id.Row]
-			switch id.Col {
-			case 0:
-				label.SetText(fmt.Sprintf("%d", game.GameID))
-			case 1:
-				label.SetText(game.Title)
-			case 2:
-				label.SetText(game.Platform)
-			case 3:
-				label.SetText(game.Genre)
-			}
-		},
-	)
-	// Custom headers for games table
-	gamesTable.UpdateHeader = func(id widget.TableCellID, obj fyne.CanvasObject) {
-		label := obj.(*widget.Label)
-		switch id.Col {
-		case 0:
-			label.SetText("ID")
-		case 1:
-			label.SetText("Titre")
-		case 2:
-			label.SetText("Plateforme")
-		case 3:
-			label.SetText("Genre")
-		}
-	}
-
-	// Build games table
-	consolesTable := widget.NewTableWithHeaders(
-		func() (int, int) {
-			return len(consoles), 4
-		},
-		func() fyne.CanvasObject {
-			return widget.NewLabel("wide content")
-		},
-		func(id widget.TableCellID, obj fyne.CanvasObject) {
-			label := obj.(*widget.Label)
-
-			// Data rows
-			console := consoles[id.Row]
-			switch id.Col {
-			case 0:
-				label.SetText(fmt.Sprintf("%d", console.ConsoleID))
-			case 1:
-				label.SetText(console.Name)
-			case 2:
-				label.SetText(console.Manufacturer)
-			case 3:
-				label.SetText(fmt.Sprintf("%d", console.Generation))
-			}
-		},
-	)
-	// Custom headers for games table
-	consolesTable.UpdateHeader = func(id widget.TableCellID, obj fyne.CanvasObject) {
-		label := obj.(*widget.Label)
-		switch id.Col {
-		case 0:
-			label.SetText("ID")
-		case 1:
-			label.SetText("Nom")
-		case 2:
-			label.SetText("Frabriquant")
-		case 3:
-			label.SetText("Gen")
-		}
-	}
-	// Set columns width
-	gamesTable.SetColumnWidth(0, 50)  // ID column - narrow
-	gamesTable.SetColumnWidth(1, 400) // Title - wide
-	gamesTable.SetColumnWidth(2, 400) // Platform - medium
-	gamesTable.SetColumnWidth(3, 100) // Genre - medium
-	consolesTable.SetColumnWidth(0, 50)
-	consolesTable.SetColumnWidth(1, 300)
-	consolesTable.SetColumnWidth(2, 300)
-	consolesTable.SetColumnWidth(3, 50)
-
-	// Hide header column
-	gamesTable.ShowHeaderColumn = false
-	consolesTable.ShowHeaderColumn = false
-
-	// Contents
+	// Build tab contents
 	accueilContent := widget.NewLabel("Dashboard")
-	jeuxContent := container.NewBorder(
-		widget.NewLabel("Jeux"),
-		nil,
-		nil,
-		nil,
-		gamesTable,
-	)
-	consolesContent := container.NewBorder(
-		widget.NewLabel("Consoles"),
-		nil,
-		nil,
-		nil,
-		consolesTable,
-	)
+	jeuxContent := buildJeuxTab(games)
+	consolesContent := buildConsolesTab(consoles)
 	accessoiresContent := widget.NewLabel("Accessoires")
 
-	// Sidebar
+	// Create sidebar with tabs
 	sidebar := container.NewAppTabs(
 		container.NewTabItem("Accueil", accueilContent),
 		container.NewTabItem("Jeux", jeuxContent),
---
+++ ui.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+// buildGamesTable creates and configures the games table
+func buildGamesTable(games []Game) *widget.Table {
+	table := widget.NewTableWithHeaders(
+		func() (int, int) {
+			return len(games), 4
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("")
+		},
+		func(id widget.TableCellID, obj fyne.CanvasObject) {
+			label := obj.(*widget.Label)
+			game := games[id.Row]
+
+			switch id.Col {
+			case 0:
+				label.SetText(fmt.Sprintf("%d", game.GameID))
+			case 1:
+				label.SetText(game.Title)
+			case 2:
+				label.SetText(game.Platform)
+			case 3:
+				label.SetText(game.Genre)
+			}
+		},
+	)
+
+	// Custom headers
+	table.UpdateHeader = func(id widget.TableCellID, obj fyne.CanvasObject) {
+		label := obj.(*widget.Label)
+		headers := []string{"ID", "Titre", "Plateforme", "Genre"}
+		label.SetText(headers[id.Col])
+	}
+
+	// Column widths
+	table.SetColumnWidth(0, 50)
+	table.SetColumnWidth(1, 400)
+	table.SetColumnWidth(2, 400)
+	table.SetColumnWidth(3, 100)
+
+	table.ShowHeaderColumn = false
+
+	return table
+}
+
+// buildConsolesTable creates and configures the consoles table
+func buildConsolesTable(consoles []Console) *widget.Table {
+	table := widget.NewTableWithHeaders(
+		func() (int, int) {
+			return len(consoles), 4
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("")
+		},
+		func(id widget.TableCellID, obj fyne.CanvasObject) {
+			label := obj.(*widget.Label)
+			console := consoles[id.Row]
+
+			switch id.Col {
+			case 0:
+				label.SetText(fmt.Sprintf("%d", console.ConsoleID))
+			case 1:
+				label.SetText(console.Name)
+			case 2:
+				label.SetText(console.Manufacturer)
+			case 3:
+				label.SetText(fmt.Sprintf("%d", console.Generation))
+			}
+		},
+	)
+
+	// Custom headers
+	table.UpdateHeader = func(id widget.TableCellID, obj fyne.CanvasObject) {
+		label := obj.(*widget.Label)
+		headers := []string{"ID", "Nom", "Frabriquant", "Gen"}
+		label.SetText(headers[id.Col])
+	}
+
+	// Column widths
+	table.SetColumnWidth(0, 50)
+	table.SetColumnWidth(1, 300)
+	table.SetColumnWidth(2, 300)
+	table.SetColumnWidth(3, 50)
+
+	table.ShowHeaderColumn = false
+
+	return table
+}
+
+// buildJeuxTab creates the complete "Jeux" tab content
+func buildJeuxTab(games []Game) fyne.CanvasObject {
+	table := buildGamesTable(games)
+	return container.NewBorder(
+		widget.NewLabel("Jeux"),
+		nil, nil, nil,
+		table,
+	)
+}
+
+// buildConsolesTab creates the complete "Consoles" tab content
+func buildConsolesTab(consoles []Console) fyne.CanvasObject {
+	table := buildConsolesTable(consoles)
+	return container.NewBorder(
+		widget.NewLabel("Consoles"),
+		nil, nil, nil,
+		table,
+	)
+}
